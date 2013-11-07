6eae25e3ddb4cef2bea0ca6c520aa57109d0e576
DS <ghostofstrider@gmail.com>
2013-11-07 20:08:49 +0200

    init

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.15'
+gem 'neo4j'
+gem 'jruby-openssl', '0.7.7'
+
+gem 'slim'
+gem 'simple_form'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'sass-rails', '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'therubyrhino'
+end
+
+group :development, :test do
+  gem 'neo4j-admin'
+end
+
+group :production do
+  # gem 'puma'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graph::Application.config.secret_token = 'b555e186a9c1d3cdd99e771307ff932c7238c2f7186018788bf26aea5e147d8916ca679ff548e95bc8e160d18e22408e3c81de53e2311d32c19ea84a7c242dc7'
